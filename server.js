eb0d0d27764f664585e1ab33a37831b14f9fb879
AbMbz <114257639+AbMbz@users.noreply.github.com>
2024-11-27 20:26:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ const SocialScience = mongoose.model("SocialScience", QuestionSchema, "social_sc
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
-});
 
 app.get("/profiles", (req, res) => {
   res.sendFile(join(__dirname, "public", "profiles.html"));
